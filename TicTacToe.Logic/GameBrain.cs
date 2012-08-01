ee69ac369a6211d632b6565d581ec280b2643d83
Matthew Leibowitz <mattleibowmail@gmail.com>
2012-08-01 19:41:04 +0200

    Re-ordered members

--- TicTacToe.Logic/GameBrain.cs
+++ TicTacToe.Logic/GameBrain.cs
@@ -4,181 +4,8 @@ namespace TicTacToe.Logic
 
     public static class GameBrain
     {
-        public static bool GetEmptySideMove(BrainResult result, Board board)
-        {
-            for (int i = 0; i < board.BoardSize;i++)
-            {
-                // is empty and is not corner
-                if (board.BoardArray[i] == 0 && board.Corners.All(c => board.GetIndex(c) != i))
-                {
-                    result.Moves.Add(board.GetMove(i));
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        public static bool GetEmptyCornerMove(BrainResult result, Board board)
-        {
-            var move = board.Corners.FirstOrDefault(m => board.BoardArray[board.GetIndex(m)] == 0);
-            if (move != null)
-            {
-                result.Moves.Add(move);
-            }
-
-            return move != null;
-        }
-
-        public static bool GetOppositeCornerMove(bool playerPiece, BrainResult result, Board board)
-        {
-            var corners = board.Corners.ToArray();
-            for (int i = 0; i < corners.Count(); i++)
-            {
-                if (board.IsSamePlayerPiece(!playerPiece, corners[i]))
-                {
-                    var o = (i % 2 == 0) ? (1) : (-1); // either the one forward or backwards
-                    var opp = corners[i + o];
-                    if (board.BoardArray[board.GetIndex(opp.X, opp.Y)] == 0)
-                    {
-                        result.Moves.Add(opp);
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        /// <summary>
-        /// Play the center.
-        /// </summary>
-        /// <remarks>
-        /// If it is the first move of the game, playing on a corner gives the 
-        /// other player more opportunities to make a mistake and may therefore
-        /// be the better choice.
-        /// However, it makes no difference between perfect players.
-        /// </remarks>
-        public static bool GetCentreMove(BrainResult result, Board board)
-        {
-            // only if the board has odd dimensions
-            if (board.BoardWidth % 2 != 0 && board.BoardHeight % 2 != 0)
-            {
-                var x = board.BoardWidth / 2;
-                var y = board.BoardHeight / 2;
-                var i = board.GetIndex(x, y);
-
-                // and if the place is empty
-                if (board.BoardArray[i] == 0)
-                {
-                    result.Moves.Add(board.GetMove(i));
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        #region Forking moves
-
-        public static bool GetBlockForkingMove(bool playerPiece, BrainResult result, Board board)
-        {
-            // can the other player create a fork?
-            var outerResult = new BrainResult();
-            var fork = GetForkingMove(!playerPiece, outerResult, board, true);
-
-            if (fork && outerResult.Moves.Count > 0)
-            {
-                var tempBoard = board.Clone();
-                for (int i = 0; i < board.BoardSize; i++)
-                {
-                    if (tempBoard.BoardArray[i] == 0)
-                    {
-                        tempBoard.BoardArray[i] = playerPiece ? 1 : -1;
-
-                        var innerResult = new BrainResult();
-                        if (GetWinningMove(playerPiece, 1, innerResult, tempBoard) // am I going to make two in a row
-                            && !outerResult.ContainsMove(tempBoard, i)) // this pos won't allow the user to fork
-                        {
-                            result.Moves.Add(board.GetMove(i));
-                            return true;
-                        }
-
-                        tempBoard.BoardArray[i] = 0;
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        /// <summary>
-        /// Fork by creating an opportunity where you can win in two ways.
-        /// </summary>
-        public static bool GetForkingMove(bool playerPiece, BrainResult result, Board board, bool getAllForks = false)
-        {
-            var tempBoard = board.Clone();
-
-            for (int i = 0; i < board.BoardSize; i++)
-            {
-                if (tempBoard.BoardArray[i] == 0)
-                {
-                    tempBoard.BoardArray[i] = playerPiece ? 1 : -1;
-
-                    var outerResult = new BrainResult();
-                    if (GetWinningMove(playerPiece, 2, outerResult, tempBoard))
-                    {
-                        var moveThatMakesTwoInARow = board.GetMove(i);
-                        var innerResult = new BrainResult();
-                        var notWin = !GetWinningMove(!playerPiece, 1, innerResult, tempBoard);
-                        var notFork = !GetOppositionForkingMove(playerPiece, tempBoard, moveThatMakesTwoInARow);
-                        if (notWin && notFork)
-                        {
-                            result.Moves.Add(moveThatMakesTwoInARow);
-                            if (!getAllForks)
-                            {
-                                return true;
-                            }
-                        }
-                    }
-
-                    tempBoard.BoardArray[i] = 0;
-                }
-            }
-
-            return getAllForks && result.Moves.Count > 0;
-        }
-
-        public static bool GetOppositionForkingMove(bool playerPiece, Board board, Move move)
-        {
-            var tempBoard = board.Clone();
-
-            // switch last to the correct player
-            tempBoard.BoardArray[board.GetIndex(move.X, move.Y)] = playerPiece ? -1 : 1;
-
-            for (int i = 0; i < board.BoardSize; i++)
-            {
-                if (tempBoard.BoardArray[i] == 0)
-                {
-                    tempBoard.BoardArray[i] = playerPiece ? 1 : -1;
-
-                    var result = new BrainResult();
-                    if (GetWinningMove(!playerPiece, 2, result, tempBoard))
-                    {
-                        return true;
-                    }
-
-                    tempBoard.BoardArray[i] = 0;
-                }
-            }
-
-            return false;
-        }
-
-        #endregion
-        
-        #region Find moves that could result in a win
-
+        #region 1. & 2. Winning Moves
+ 
         /// <summary>
         /// If a player has two in a row, play the third to get three in a row.
         /// </summary>
@@ -313,6 +140,195 @@ namespace TicTacToe.Logic
             }
 
             // no place
+            return false;
+        }
+
+        #endregion
+
+        #region 3. & 4. Forking Moves
+
+        public static bool GetBlockForkingMove(bool playerPiece, BrainResult result, Board board)
+        {
+            // can the other player create a fork?
+            var outerResult = new BrainResult();
+            var fork = GetForkingMove(!playerPiece, outerResult, board, true);
+
+            if (fork && outerResult.Moves.Count > 0)
+            {
+                var tempBoard = board.Clone();
+                for (int i = 0; i < board.BoardSize; i++)
+                {
+                    if (tempBoard.BoardArray[i] == 0)
+                    {
+                        tempBoard.BoardArray[i] = playerPiece ? 1 : -1;
+
+                        var innerResult = new BrainResult();
+                        if (GetWinningMove(playerPiece, 1, innerResult, tempBoard) // am I going to make two in a row
+                            && !outerResult.ContainsMove(tempBoard, i)) // this pos won't allow the user to fork
+                        {
+                            result.Moves.Add(board.GetMove(i));
+                            return true;
+                        }
+
+                        tempBoard.BoardArray[i] = 0;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Fork by creating an opportunity where you can win in two ways.
+        /// </summary>
+        public static bool GetForkingMove(bool playerPiece, BrainResult result, Board board, bool getAllForks = false)
+        {
+            var tempBoard = board.Clone();
+
+            for (int i = 0; i < board.BoardSize; i++)
+            {
+                if (tempBoard.BoardArray[i] == 0)
+                {
+                    tempBoard.BoardArray[i] = playerPiece ? 1 : -1;
+
+                    var outerResult = new BrainResult();
+                    if (GetWinningMove(playerPiece, 2, outerResult, tempBoard))
+                    {
+                        var moveThatMakesTwoInARow = board.GetMove(i);
+                        var innerResult = new BrainResult();
+                        var notWin = !GetWinningMove(!playerPiece, 1, innerResult, tempBoard);
+                        var notFork = !GetOppositionForkingMove(playerPiece, tempBoard, moveThatMakesTwoInARow);
+                        if (notWin && notFork)
+                        {
+                            result.Moves.Add(moveThatMakesTwoInARow);
+                            if (!getAllForks)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+
+                    tempBoard.BoardArray[i] = 0;
+                }
+            }
+
+            return getAllForks && result.Moves.Count > 0;
+        }
+
+        public static bool GetOppositionForkingMove(bool playerPiece, Board board, Move move)
+        {
+            var tempBoard = board.Clone();
+
+            // switch last to the correct player
+            tempBoard.BoardArray[board.GetIndex(move.X, move.Y)] = playerPiece ? -1 : 1;
+
+            for (int i = 0; i < board.BoardSize; i++)
+            {
+                if (tempBoard.BoardArray[i] == 0)
+                {
+                    tempBoard.BoardArray[i] = playerPiece ? 1 : -1;
+
+                    var result = new BrainResult();
+                    if (GetWinningMove(!playerPiece, 2, result, tempBoard))
+                    {
+                        return true;
+                    }
+
+                    tempBoard.BoardArray[i] = 0;
+                }
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region 5. Play Centre
+
+        /// <summary>
+        /// Play the center.
+        /// </summary>
+        /// <remarks>
+        /// If it is the first move of the game, playing on a corner gives the 
+        /// other player more opportunities to make a mistake and may therefore
+        /// be the better choice.
+        /// However, it makes no difference between perfect players.
+        /// </remarks>
+        public static bool GetCentreMove(BrainResult result, Board board)
+        {
+            // only if the board has odd dimensions
+            if (board.BoardWidth % 2 != 0 && board.BoardHeight % 2 != 0)
+            {
+                var x = board.BoardWidth / 2;
+                var y = board.BoardHeight / 2;
+                var i = board.GetIndex(x, y);
+
+                // and if the place is empty
+                if (board.BoardArray[i] == 0)
+                {
+                    result.Moves.Add(board.GetMove(i));
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region 6. Play Opposite Corner
+
+        public static bool GetOppositeCornerMove(bool playerPiece, BrainResult result, Board board)
+        {
+            var corners = board.Corners.ToArray();
+            for (int i = 0; i < corners.Count(); i++)
+            {
+                if (board.IsSamePlayerPiece(!playerPiece, corners[i]))
+                {
+                    var o = (i % 2 == 0) ? (1) : (-1); // either the one forward or backwards
+                    var opp = corners[i + o];
+                    if (board.BoardArray[board.GetIndex(opp.X, opp.Y)] == 0)
+                    {
+                        result.Moves.Add(opp);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region 7. Play Empty Corner
+        
+        public static bool GetEmptyCornerMove(BrainResult result, Board board)
+        {
+            var move = board.Corners.FirstOrDefault(m => board.BoardArray[board.GetIndex(m)] == 0);
+            if (move != null)
+            {
+                result.Moves.Add(move);
+            }
+
+            return move != null;
+        }
+
+        #endregion
+
+        #region 8. Play Empty Side
+
+        public static bool GetEmptySideMove(BrainResult result, Board board)
+        {
+            for (int i = 0; i < board.BoardSize;i++)
+            {
+                // is empty and is not corner
+                if (board.BoardArray[i] == 0 && board.Corners.All(c => board.GetIndex(c) != i))
+                {
+                    result.Moves.Add(board.GetMove(i));
+                    return true;
+                }
+            }
+
             return false;
         }
 
